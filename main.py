c2dbb66e22950e0abf3c962d8ae62da555819172
klaus kapllani <klaus.kapllani@edu.esiee.fr>
2024-11-08 22:13:50 +0000

    Implémentation des fonctions read_data, get_list_k, get_first, get_last, get_max, get_min, get_sum

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#### Imports et définition des variables globales
-import random
+# main.py
+"""
+author = klaus.kapllani@esiee.fr
+"""
 
 FILENAME = "listes.csv"
-
-#### Fonctions secondaires
 
 def read_data(filename):
     """retourne le contenu du fichier <filename>
@@ -15,39 +15,94 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        l = [[int(v) for v in ligne.strip().split(';')] for ligne in f.readlines()]
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """retourne la k-ème liste de data
+
+    Args:
+        data (list): une liste de listes
+
+    Returns:
+        list: la k-ème liste
+    """
+    return data[k]
 
 def get_first(l):
-    return None
+    """retourne le premier élément d'une liste
+
+    Args:
+        l (list): une liste
+
+    Returns:
+        int : le premier élément de la liste
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """retourne le dernier élément d'une liste
+
+    Args:
+        l (list): une liste
+
+    Returns:
+        int : le dernier élément de la liste
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """retourne le maximum liste
+
+    Args:
+        l (list): une liste
+
+    Returns:
+        int : le maximum de la liste
+    """
+    return max(l)
 
 def get_min(l):
-    return None
+    """retourne le minimum d'une liste
+
+    Args:
+        l (list): une liste
+
+    Returns:
+        int : le minimum de la liste
+    """
+    return min(l)
 
 def get_sum(l):
-    return None
+    """retourne la somme d'une liste
 
+    Args:
+        l (list): une liste
 
-#### Fonction principale
-
+    Returns:
+        int : la somme de la liste
+    """
+    return sum(l)
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    """
+    Fonction principale qui lit le fichier <FILENAME>,
+    affiche les éléments de la k-eme liste, puis son premier élément,
+    son dernier élément, son élément max, son élément min et enfin la somme de celle-ci.
 
+    Returns :
+        rien
+    """
+    data = read_data(FILENAME)
+    k = 37
+    print(k, get_list_k(data, 37))
+    print("Le premier élément de la ", k, "ème liste est :", get_first(get_list_k(data, 37)))
+    print("Le dernier élément de la ", k, "ème liste est :", get_last(get_list_k(data, 37)))
+    print("L'élément max de la ", k, "ème liste est :", get_max(get_list_k(data, 37)))
+    print("L'élément min de la ", k, "ème liste est :", get_min(get_list_k(data, 37)))
+    print("La somme de la ", k, "ème liste est :", get_sum(get_list_k(data, 37)))
 
 if __name__ == "__main__":
     main()
